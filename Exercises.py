93b7763c983b8a8ac1ad4fba659f113811f8ac8d
trobinson9 <109588774+trobinson9@users.noreply.github.com>
2022-07-24 17:01:14 +0100

    Add files via upload

---
+++ Exercises.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+#  Make pd series from np
+mylist = list('abcedfghijklmnopqrstuvwxyz')
+myarr = np.arange(26)
+mydict = dict(zip(mylist, myarr))
+
+ser1 = pd.Series(mylist)
+ser2 = pd.Series(myarr)
+ser3 = pd.Series(mydict)
+
+print(ser3.head())
